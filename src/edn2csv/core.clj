4afb7d90d6078c3985d27ab33ab9c4fdfc86c5c6
DanishMalik <malik083.morris.umn.edu>
2018-05-07 22:45:25 -0500

    Individual and ParentOf_edges nodes files are working.

--- src/edn2csv/core.clj
+++ src/edn2csv/core.clj
@@ -3,13 +3,17 @@
            [clojure.edn :as edn]
            [clojure.java.io :as io]
            [clojure.pprint :as pp]
+           [clojure.set :as set]
            [iota]
            [me.raynes.fs :as fs])
   (:gen-class))
 
 ; The header line for the Individuals CSV file
 (def individuals-header-line "UUID:ID(Individual),Generation:int,Location:int,:LABEL")
-(def semantics-header-line "UUID:ID(Semantics),TotalError:int,:LABEL")
+(def parentof-edges-header-line ":START_ID(Individual),GeneticOperator,:END_ID(Individual),:TYPE")
+
+; atomic holder
+(def sem-error (atom {}))
 
 ; Ignores (i.e., returns nil) any EDN entries that don't have the
 ; 'clojure/individual tag.
@@ -25,105 +29,64 @@
 (defn safe-println [output-stream & more]
   (.write output-stream (str (clojure.string/join "," more) "\n")))
 
+(defn print-single-parent-edge [out-file child parent]
+    (apply safe-println out-file (concat [parent] child)))
+
 ; This prints out the relevant fields to the CSV filter
 ; and then returns 1 so we can count up how many individuals we processed.
 ; (The counting isn't strictly necessary, but it gives us something to
 ; fold together after we map this across the individuals; otherwise we'd
 ; just end up with a big list of nil's.)
 (defn print-individual-to-csv
-  [csv-file line]
+  [individual-out-file line]
   (as-> line $
     (map $ [:uuid :generation :location])
     (concat $ ["Individual"])
-    (apply safe-println csv-file $))
+    (apply safe-println individual-out-file $))
   1)
 
-(defn uuid [] (str (java.util.UUID/randomUUID))
+(defn print-parentOf-edges-to-csv
+  [parentof-edges-out-file line]
+ (as-> line $
+      (map $ [:genetic-operators :uuid])
+      (concat $ ["PARENT_OF"])
+      (doall (map (partial print-single-parent-edge parentof-edges-out-file $)(line :parent-uuids)))))
 
-(defn print-semantics-to-csv
-  [csv-file line]
-  (as-> line $
-    (concat $ [(uuid)])
-    (map $ [:total-errors])
-    (concat $ ["Semantics"] )
-    (apply safe-println csv-file $))
-  1)
+(defn print-individual-and-parentOf-to-csv
+  [individual-out-file parentof-edges-out-file line]
+  (do 
+   (swap! sem-error (set/union @sem-error #{line}))
+   (print-individual-to-csv individual-out-file line)
+   (print-parentOf-edges-to-csv parentof-edges-out-file line)
+   1))
+      
 
-(defn edn->csv-sequential [edn-file csv-file]
-  (with-open [out-file (io/writer csv-file)]
-    (safe-println out-file individuals-header-line)
-    (->>
-      (line-seq (io/reader edn-file))
-      ; Skip the first line because it's not an individual
-      (drop 1)
-      (map (partial edn/read-string {:default individual-reader}))
-      (map (partial print-individual-to-csv out-file))
-      (map (partial print-semantics-to-csv out-file))
-      (reduce +)
-      )))
+(defn build-csv-filename
+    [edn-filename type]
+    (str (fs/parent edn-filename)
+         "/"
+         (fs/base-name edn-filename ".edn")
+         "_"
+         type
+         ".csv"))
 
-(defn edn->csv-pmap [edn-file csv-file]
-  (with-open [out-file (io/writer csv-file)]
-    (safe-println out-file individuals-header-line)
-    (->>
-      (line-seq (io/reader edn-file))
-      ; Skip the first line because it's not an individual
-      (drop 1)
-      (pmap (fn [line]
-        (print-individual-to-csv out-file (edn/read-string {:default individual-reader} line))
-        1))
-      count
-      )))
-
-(defn edn->csv-reducers [edn-file csv-file]
-  (with-open [out-file (io/writer csv-file)]
-    (safe-println out-file individuals-header-line)
+(defn edn->csv-reducers [edn-file]
+  (with-open [individual-out-file (io/writer (build-csv-filename edn-file "Individual"))
+             parentof-edges-out-file (io/writer (build-csv-filename edn-file "ParentOf_edges"))]
+    (safe-println individual-out-file individuals-header-line)
+    (safe-println parentof-edges-out-file  parentof-edges-header-line)
     (->>
       (iota/seq edn-file)
       (r/map (partial edn/read-string {:default individual-reader}))
-      ; This eliminates empty (nil) lines, which result whenever
-      ; a line isn't a 'clojush/individual. That only happens on
-      ; the first line, which is a 'clojush/run, but we still need
-      ; to catch it. We could do that with `r/drop`, but that
-      ; totally kills the parallelism. :-(
       (r/filter identity)
-      (r/map (partial print-individual-to-csv out-file))
-      (r/fold +)
-      )))
+      (r/map (partial print-individual-and-parentOf-to-csv individual-out-file parentof-edges-out-file))
+      (r/fold +))))
 
-(defn build-individual-csv-filename
-  [edn-filename strategy]
-  (str (fs/parent edn-filename)
-       "/"
-       (fs/base-name edn-filename ".edn")
-       (if strategy
-         (str "_" strategy)
-         "_sequential")
-       "_Individuals.csv")
-  
-(defn build-semantics-csv-filename
-  [edn-filename strategy]
-  (str (fs/parent edn-filename)
-       "/"
-       (fs/base-name edn-filename ".edn")
-       (if strategy
-         (str "_" strategy)
-         "_sequential")
-       "_Semantics.csv")
-  
+
 (defn -main
-  [edn-filename & [strategy]]
-  (let [semantics-csv-file (build-semantics-csv-filename edn-filename strategy)]
-
+    [edn-filename]
     (time
-      (condp = strategy
-       "sequential" (edn->csv-sequential edn-filename semantics-csv-file)
-       ; "pmap" (edn->csv-pmap edn-filename individual-csv-file)
-       ; "pmap" (edn->csv-pmap edn-filename semantics-csv-file)
-      ;  "reducers" (edn->csv-reducers edn-filename individual-csv-file)
-       ; "reducers" (edn->csv-reducers edn-filename semantics-csv-file)
-       ; (edn->csv-sequential edn-filename individual-csv-file))))
-        (edn->csv-sequential edn-filename semantics-csv-file))))
-  ; Necessary to get threads spun up by `pmap` to shutdown so you get
-  ; your prompt back right away when using `lein run`.
-  (shutdown-agents))
+        (edn->csv-reducers edn-filename))
+    ; Necessary to get threads spun up by `pmap` to shutdown so you get
+    ; your prompt back right away when using `lein run`.
+    (shutdown-agents))
